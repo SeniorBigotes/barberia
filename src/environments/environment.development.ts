9731ba3ee985b493da15730c61030a96716404f5
mauricio_puente <mauriciopuente.pineda2000@gmail.com>
2025-07-26 14:53:01 -0600

    Primeros cambios

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,9 +2,6 @@ export const environment = {
   prod: false,
   phone: '5587095927',
   email: 'elbarbitascdmx@gmail.com',
-  coordinateX: 19.345232888592964,
-  coordinateY: -99.13990930957368,
+  coordinateX: 19.2952211,
+  coordinateY: -99.1527444,
 };
-
-
-// ,
